6c62977c7448e33cfcf7e268b7d433d67c0cd5fb
ZacMcleod <zacamcleod33@gmail.com>
2023-06-19 12:07:16 -0700

    Test

--- app.py
+++ app.py
@@ -30,13 +30,42 @@ class Car(db.Model):
     model = db.Column(db.String(255), nullable = False)
     year = db.Column(db.Integer)
 
+    def repr (self):
+        return f'{self.year} {self.make} {self.model}'
 
 # Schemas
+class CarSchema (ma. Schema):
+    class Meta:
+        fields = ("id", "make", "model", "year")
 
-
+car_schema = CarSchema()
+cars_schema = CarSchema (many=True)
 
 # Resources
+class CarListResource(Resource):
+    def get(self):
+        all_cars = Car.query.all()
+        return cars_schema.dump(all_cars)
+    
+    def post(self):
+        print (request)
+        new_car = Car(
+            make=request.json ['make'], 
+            model=request.json ['model'], 
+            year=request.json ['year' ]
+        )   
+        db.session.add(new_car)
+        db.session.commit()
+        return car_schema.dump(new_car), 201
 
-
-
+class CarResource(Resource):
+    def get(self, car_id):
+        car_from_db = Car.query.get_or_404(car_id)
+        return car_schema.dump(car_from_db)
+    def delete(self, car_id):
+        car_from_db = Car.query.get_or_404(car_id)
+        db.session.delete(car_from_db)
+        return '', 204
 # Routes
+api.add_resource (CarListResource, '/api/cars')
+api.add_resource(CarResource, '/api/cars/<int:car_id>')
